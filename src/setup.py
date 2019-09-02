043890404f8e0167109ef3ec5ad6a590bd5f22f7
Aaron Pulver <apulver@esri.com>
2019-09-02 13:42:33 -0400

    update setup.py

--- src/setup.py
+++ src/setup.py
@@ -1,19 +1,19 @@
 from distutils.core import setup
 
 setup(name='Allagash',
-    version='0.0.1',
-    description='A Python Spatial Optimization Library',
-    author='Aaron Pulver',
-    author_email='apulverizer@gmail.com',
-    url='https://github.com/apulverizer/allagash',
-    packages=['allagash'],
-    license='MIT',
-    install_requires=[
-        'pulp~=1.6.1',
-        'geopandas~=0.4.1'
-    ],
-    classifiers=[
-      'Intended Audience :: Developers/Researchers',
-      'Programming Language :: Python :: 3.6'
-    ]
- )
+      version='0.0.1',
+      description='A Python Spatial Optimization Library',
+      author='Aaron Pulver',
+      author_email='apulverizer@gmail.com',
+      url='https://github.com/apulverizer/allagash',
+      packages=['allagash'],
+      license='MIT',
+      install_requires=[
+          'pulp~=1.6.1',
+          'geopandas~=0.4.1'
+      ],
+      classifiers=[
+          'Intended Audience :: Developers/Researchers',
+          'Programming Language :: Python :: 3'
+      ]
+      )
