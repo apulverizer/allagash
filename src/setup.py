0edd3c2a4c8240c22e14ee4a91c7d004358d6dd5
Aaron Pulver <apulver@esri.com>
2019-09-07 16:39:16 -0400

    test

--- src/setup.py
+++ src/setup.py
@@ -1,7 +1,27 @@
+import codecs
+import os
+import re
 from setuptools import setup
 
+here = os.path.abspath(os.path.dirname(__file__))
+
+
+def read(*parts):
+    with codecs.open(os.path.join(here, *parts), 'r') as fp:
+        return fp.read()
+
+
+def find_version(*file_paths):
+    version_file = read(*file_paths)
+    version_match = re.search(r"^__version__ = ['\"]([^'\"]*)['\"]",
+                              version_file, re.M)
+    if version_match:
+        return version_match.group(1)
+    raise RuntimeError("Unable to find version string.")
+
+
 setup(name='Allagash',
-      version='0.0.6',
+      version=find_version("allagash", "__init__.py"),
       description='A Python Spatial Optimization Library',
       long_description='A spatial optimization library for covering problems.',
       author='Aaron Pulver',
