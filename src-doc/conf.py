d844f0379819556235ac2c24d8ae736e13e7e082
Aaron Pulver <apulver@esri.com>
2019-09-07 21:07:57 -0400

    update conf file

--- src-doc/conf.py
+++ src-doc/conf.py
@@ -13,7 +13,25 @@
 # import os
 # import sys
 # sys.path.insert(0, os.path.abspath('.'))
-import allagash
+import codecs
+import os
+import re
+
+here = os.path.abspath(os.path.dirname(__file__))
+
+
+def read(*parts):
+    with codecs.open(os.path.join(here, *parts), 'r') as fp:
+        return fp.read()
+
+
+def find_version(*file_paths):
+    version_file = read(*file_paths)
+    version_match = re.search(r"^__version__ = ['\"]([^'\"]*)['\"]",
+                              version_file, re.M)
+    if version_match:
+        return version_match.group(1)
+    raise RuntimeError("Unable to find version string.")
 
 # -- Project information -----------------------------------------------------
 
@@ -22,7 +40,7 @@ copyright = '2019, Aaron Pulver'
 author = 'Aaron Pulver'
 
 # The full version, including alpha/beta/rc tags
-release = allagash.__version__
+release = find_version(r"../src/allagash", "__init__.py")
 
 
 # -- General configuration ---------------------------------------------------
