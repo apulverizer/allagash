ca5cf4277db65d78e8b6af4b1e3e041d1cbdf48e
Aaron Pulver <apulver@esri.com>
2021-12-24 20:13:34 -0500

    bump version to 0.2.2

--- src/allagash/__init__.py
+++ src/allagash/__init__.py
@@ -1,4 +1,4 @@
 from .problem import Problem, UnboundedException, UndefinedException, InfeasibleException, NotSolvedException
 from .coverage import Coverage
 
-__version__ = "0.2.1"
+__version__ = "0.2.2"
